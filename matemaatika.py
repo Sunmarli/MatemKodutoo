f7b8b63b9dffd069470564a5ff5568493cafcfa6
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2022-12-11 19:59:51 +0200

    Add files via upload

---
+++ matemaatika.py
@@ -0,0 +1,51 @@
+from math import *
+from random import *
+#В программе случайным образом "задаются" примеры, с учетом сложности провряемых знаний.
+#После введенного пользователем ответа, проверяется его правильностью.
+
+
+answers = 5
+print("Проверка знаний\n")
+Tase=int(input("Choose tase, 1,2 or 3?"))
+if Tase==1:
+        for i in range (5):
+            first=randint(0,10)
+            second=randint(0,10)
+            res=first+second
+            print(f"{first} + {second} =", end=" ")
+            answer = int(input(""))
+            if answer !=res:
+                print("Ошибка!")
+                answers -= 1
+elif Tase==2:
+        for i in range (5):
+            first=randint(0,100)
+            second=randint(0,100)
+            res=first-second
+            print(f"{first} - {second} =", end=" ")
+            answer = int(input(""))
+            if answer !=res:
+                print("Ошибка!")
+                answers -= 1
+elif Tase==3:
+        for i in range (5):
+            first=randint(0,100)
+            second=randint(0,100)
+            res=first*second
+            print(f"{first} * {second} =", end=" ")
+            answer = int(input(""))
+            if answer !=res:
+                print("Ошибка!")
+                answers -= 1
+
+print(f"Правильных ответов: {answers}")
+ 
+if 0 <= answers <= 2:
+    print("Оценка: плохо")
+if 2 <= answers <= 3:
+    print("Оценка: хорошо")
+if 3 <= answers <= 4:
+    print("Оценка: удовлетворительно")
+if 4 <= answers <= 5:
+    print("Оценка: отлично")
+exit = input()
